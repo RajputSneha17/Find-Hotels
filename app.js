545dd1a8527d352574fb503fa4a394b6f020fc9b
RajputSneha17 <sneharajput63154@gmail.com>
2025-07-27 23:00:43 +0530

    new

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -9,13 +9,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/reviews.js");
-const session = require('express-session');
+const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -24,13 +24,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
-
 const dbUrl = process.env.ATLASDB_URL;
 // MongoDB connection
 async function main() {
-   await mongoose.connect(dbUrl);
+  await mongoose.connect(dbUrl);
 }
-
 
 main()
   .then(() => console.log("Connected to DB"))
@@ -39,17 +37,17 @@ main()
     process.exit(1);
   });
 
-  const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto:{
-      secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
-  })
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
 
-  store.on("error", () => {
-    console.log("ERROR  in MONGO SESSION STORE", err);
-  });
+store.on("error", (err) => {
+  console.log("ERROR  in MONGO SESSION STORE", err);
+});
 
 const sessionOptions = {
   store,
@@ -72,7 +70,6 @@ passport.use(new LocalStrategy(User.authenticate())); // Fixed LocalStrategy
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 passport.use(User.createStrategy());
-
 
 // Flash messages middleware
 app.use((req, res, next) => {
@@ -102,5 +99,5 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);  // Fixed template literal
+  console.log(`Server running on port http://localhost:${PORT}`); // Fixed template literal
 });
